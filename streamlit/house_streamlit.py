756faf6faa7cea85fd9e172cfa3909f55865af20
Andrea Haritcalde <andrea.haritcalde@gmail.com>
2024-03-27 16:24:39 +0100

    Finish deployable Streamlit

--- streamlit/house_streamlit.py
+++ streamlit/house_streamlit.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+from streamlit_extras.stylable_container import stylable_container
 import plotly.express as px
 from plots.plots import Plots
 import requests
@@ -9,6 +10,10 @@ import requests
 def main():
 
 
+
+    # Initializes data for dropdown menus
+
+    #State of construction, dict makes options prettier
     states = {
         "As new": "AS_NEW",
         "Good": "GOOD",
@@ -17,61 +22,94 @@ def main():
         "To renovate": "TO_RENOVATE",
         "To restore": "TO_RESTORE"
     }
-
+    # Belgian provinces
     districts =  ["Aalst", "Antwerp", "Arlon", "Ath", "Bastogne", "Brugge", "Brussels", "Charleroi", "Dendermonde", 
 "Diksmuide", "Dinant", "Eeklo", "Gent", "Halle-Vilvoorde", "Hasselt", "Huy", "Ieper", "Kortrijk", "Leuven", "Liège", 
 "Maaseik", "Marche-en-Famenne", "Mechelen", "Mons", "Mouscron", "Namur", "Neufchâteau", "Nivelles", "Oostend", "Oudenaarde", 
 "Philippeville", "Roeselare", "Sint-Niklaas", "Soignies", "Thuin", "Tielt", "Tongeren", "Tournai", "Turnhout", "Verviers", 
 "Veurne", "Virton", "Waremme"]
 
-    epcs = {"A++": 9 , "A+": 8, "A": 7, "B":6, "C":5, "D":4, "E":3, "F":2, "G": 1}
+    epcs = ["A++", "A+", "A", "B", "C", "D", "E", "F", "G"]
 
+    # Writes app header with html
     html = """
-    <div style = "background-color: lightblue ; padding: 16px>
-    <h2 style = "color: black; text-align: center;"> House Price Prediction</h2>
-    </div>
+    <div style = "background-color: rgb(255, 75, 75); padding: 10px; box-sizing: border-box;
+    border-radius: 15px;">
+    <h2 style = "color: white; text-align: center;"> House Price Prediction 🏠</h2>
+    </div>  
     """
-
     st.markdown(html, unsafe_allow_html = True)
-
     st.write ("")
 
+    # Begins selection tools
     district = st.selectbox(label = "In which district is the house located?", options = districts)
-    area_total = st.number_input(label = "What is the total area of the house (m²)?", min_value = 50.0, max_value= 1500.0, step = 0.5)
-    living_area = st.number_input(label = "What is the total living area (m²)?", min_value = 50.0, max_value= 350.0, step = 0.5 )
-
+    area_total = st.number_input(label = "What is the total area of the house (m²)?", value = 100.0, min_value = 50.0, max_value= 1500.0, step = 0.5)
+    living_area = st.number_input(label = "What is the total living area (m²)?", value = 100.0, min_value = 50.0, max_value= 350.0, step = 0.5 )
     select_state = st.selectbox (label = "What is the state of the house?", options= list(states.keys()), index = 1)
-    select_epc = st.select_slider(label ="What is the house's EPC?", options = (list(epcs.keys())), value = "A")
+    select_epc = st.select_slider(label ="What is the house's EPC?", options = epcs, value = "A")
     bedrooms = st.slider(label = "What is the number of bedrooms?", min_value=1, max_value= 15)
     bathrooms = st.slider(label = "What is the number of bathrooms?", min_value=1, max_value= 10)
     facades = st.slider(label = "How many exposed façades?", min_value=1, max_value=4)
 
-    st.write('Does the house have a garden?')
-    has_garden = st.toggle(label = "With garden" , value=False, label_visibility="collapsed")
-    if has_garden:
-        st.write('It has a garden.')
+    # For boolean variables
+    # Divides them into columns for better redability 
+    col_1, col_2 = st.columns(2)
+
+    with col_1:
+        # Makes a border into the column
+        with stylable_container(
+        key="container_with_border",
+        css_styles="""
+            {
+                border: 1px solid rgba(49, 51, 63, 0.2);
+                border-radius: 0.5rem;
+                padding: calc(1em - 1px)
+            }
+            """,):        
+
+            # Starts the toggle buttoms with text confirmation of choice
+            st.write('Does the house have a garden?')
+            has_garden = st.toggle(label = "With garden" , value=False, label_visibility="collapsed")
+            if has_garden:
+                st.write('It has a garden.')
+
+            
+            st.write('Does the house have at least one terrace?')
+            has_terrace = st.toggle(label = "With terrace" , value=False, label_visibility="collapsed")
+            if has_terrace:
+                st.write('It has at least one terrace.')
+
+            st.write('Does the house have an attic?')   
+            has_attic = st.toggle(label = "With attic" , value=False, label_visibility="collapsed")
+            if has_attic:
+                st.write('It has an attic.')
+
+    # Second column of toggles 
+    with col_2:
+        # Makes border for the column
+        with stylable_container(
+        key="container_with_border_2",
+        css_styles="""
+            {
+                border: 1px solid rgba(49, 51, 63, 0.2);
+                border-radius: 0.5rem;
+                padding: calc(1em - 1px)
+            }
+            """,):
+
+            # Start toggles
+            st.write('Does the house have an equipped kitchen?')
+            has_equipped_kitchen = st.toggle(label = "With equipepd kitchen" , value=False, label_visibility="collapsed")
+            if has_equipped_kitchen:
+                st.write('It has an equipped kitchen.')
 
 
-    st.write('Does the house have an equipped kitchen?')
-    has_equipped_kitchen = st.toggle(label = "With equipepd kitchen" , value=False, label_visibility="collapsed")
-    if has_equipped_kitchen:
-        st.write('It has an equipped kitchen.')
+            st.write('Does the house have a basement?')   
+            has_basement = st.toggle(label = "With basement" , value=False, label_visibility="collapsed")
+            if has_basement:
+                st.write('It has a basement.')
 
-    st.write('Does the house have at least one terrace?')
-    has_terrace = st.toggle(label = "With terrace" , value=False, label_visibility="collapsed")
-    if has_terrace:
-        st.write('It has at least one terrace.')
-
-    st.write('Does the house have an attic?')   
-    has_attic = st.toggle(label = "With attic" , value=False, label_visibility="collapsed")
-    if has_attic:
-        st.write('It has an attic.')
-
-    st.write('Does the house have a basement?')   
-    has_basement = st.toggle(label = "With basement" , value=False, label_visibility="collapsed")
-    if has_basement:
-        st.write('It has a basement.')
-
+    # Builts json data to sent to API
     data = {
             "district": district,
             "state_construction": states[select_state],
@@ -87,41 +125,58 @@ def main():
             "epc": select_epc,
             "area_total": area_total
             }
-        
-    if st.button(label = "Predict Price"):
+
+    #Starts button to run prediction
+
+
+    
+    if st.button(label = "Predict Price", use_container_width = True, type="primary"):
+        # Send post request to API with json
         URL = "https://immo-eliza-deployment-o9qq.onrender.com/predict"
         r = requests.post(URL, json = data)
         result = r.json()
         predict = result["prediction"]
-        print (result)
-    
+        # Prints the result from the API    
         st.success(f"The predicted price for your house is €{int(predict)}.")
 
-        st.write (f"Your house compared to others in {district}:")
-        # Start plots      
 
-        
+        # Starts interesting plots for the case
+        st.divider()
+        # Sub header to announce the plots with a custom HTML
+        html = """
+    <div style = "background-color: rgb(255, 75, 75); padding: 10px; box-sizing: border-box;
+    border-radius: 15px;">
+    <h4 style = "color: white; text-align: center;"> Your house compared to others in {district}</h4>
+    </div>  
+    """.format(district=district)
+        st.markdown(html, unsafe_allow_html = True)
+        st.write ("")
 
-
+        # Prepare plots from class in plots.py        
         data_plot = "./plots/cleaned_houses.csv"
         plots = Plots(file = data_plot, district = district )
-        price_plot = plots.price_sqm(prediction = predict, living_area = living_area)
+
+        # Puts plots into a container with tabs for better UX
+        with st.container():
+            tab1, tab2= st.tabs(["Price/living Area", "Rooms distribution"])
+            with tab1:
+                # Plot for price and living area via plotly
+                price_plot = plots.price_sqm(prediction = predict, living_area = living_area)                
+                st.plotly_chart(price_plot , use_container_width=True, sharing="streamlit", theme="streamlit") 
+            with tab2:
+                # Plot two pie charts one in each column  
+                col1, col2 = st.columns(2)
+
+                with col1: 
+                    # Bedroom piechart
+                    pie_bedroom = plots.pie_chart(feature="bedrooms", feature_name="Bedrooms", home_value= bedrooms)
+                    st.plotly_chart(pie_bedroom , use_container_width=True, sharing="streamlit", theme="streamlit")
+                with col2:
+                    # Bathroom piechart
+                    pie_bathroom = plots.pie_chart(feature="bathrooms", feature_name="Bathrooms", home_value= bathrooms)
+                    st.plotly_chart(pie_bathroom , use_container_width=True, sharing="streamlit", theme="streamlit")
         
-        st.plotly_chart(price_plot , use_container_width=True, sharing="streamlit", theme="streamlit")      
-
-        col1, col2 = st.columns(2)
-
-        with col1: 
-
-            # Bedroom pie
-            pie_bedroom = plots.pie_chart(feature="bedrooms", feature_name="Bedrooms", home_value= bedrooms)
-            st.plotly_chart(pie_bedroom , use_container_width=True, sharing="streamlit", theme="streamlit")
-        with col2:
-            # Bathroom pie
-            pie_bathroom = plots.pie_chart(feature="bathrooms", feature_name="Bathrooms", home_value= bathrooms)
-            st.plotly_chart(pie_bathroom , use_container_width=True, sharing="streamlit", theme="streamlit")
     
- 
 
 
 if __name__ == "__main__":
--- streamlit/plots/plots.py
+++ streamlit/plots/plots.py
@@ -4,36 +4,51 @@ import plotly.graph_objects as go
 
 
 class Plots:
-    def __init__(self, file, district): 
+    """
+    Makes plots based on predicted value of a house and its properties.
+    Arguments:
+    file (str): source file with dataframe of houses to build plots.
+    district (str): district of the house whose value was predicted.
+    """
+    def __init__(self, file: str, district: str) -> None: 
         self.district = district             
-
+        # Reads CSV with houses
         dataframe = pd.read_csv(file)
+        # Create column of price/m2
         dataframe ["price_sqm_total"] = dataframe ["price"]/dataframe ["living_area"]
+        # Stores unmodified df
         self.df_complete = dataframe
-
+        # Stores df for specific district
         mask = self.df_complete["district"] == district
         self.df_district = self.df_complete[mask]
 
     @staticmethod
     def thousants_point(x: int) -> str:  
+        """Adds point as thousant separator to a number"""
         return str('{:,}'.format(round(x)).replace(',','.'))
     
-    def price_sqm (self, prediction: float, living_area: float):
+    def price_sqm (self, prediction: float, living_area: float) -> None:
+        """Builds a histogram via plotly of house count versus price per sqm.
+        Adds also two vertical lines:
+        One with the predicted price/ m2 of the house.
+        Another for the average price/sqm of the province"""
+        # Dataframe about the district and district to be used
         subset_district = self.df_district
         district = self.district
-
+        # Calculates average price of the district per sqm
         average_price = round(subset_district["price_sqm_total"].mean())  
 
 
-        # text prediction
+        # Calculares price/sqm and sets up a text for the vertical line
         price_sqm_predict = int(prediction/living_area)
         text_pred = "Your house €" + self.thousants_point(price_sqm_predict)  + "/m²"
 
-        # Starts setting up the histogram
-                
-        fig = px.histogram(subset_district,  x="price_sqm_total", 
+        # Starts setting up the histogram                
+        fig = px.histogram(subset_district,  x="price_sqm_total", opacity = 0.7,
         labels ={"price_sqm_total": f"Price per living area (€/m²) in {district}"}).update_layout(yaxis_title=f"Houses Count") 
        
+        # Add vertical line and text with a arrow for the average price of the district
+        # Done this way because otherwise the texts go over each other or the axis numbers etc
         fig.add_shape(type="line", x0=average_price, x1=average_price,  yref="paper", y0=0.1, y1=0.9, line=dict(color="Green",width=2))
         fig.add_annotation(x=average_price,  y= 0,
             text=f"Average: €{average_price}/m²",
@@ -43,6 +58,7 @@ class Plots:
             ax = 20,
             ay = 20
             )
+        # Add vertical line with text for the prediction
         fig.add_shape(type="line", x0=price_sqm_predict, x1=price_sqm_predict,  yref="paper", y0=0.1, y1=1, line=dict(color="firebrick",width=3))
         fig.add_annotation(x=price_sqm_predict,
             text=f"Your house: €{price_sqm_predict}/m²",            
@@ -52,19 +68,25 @@ class Plots:
             showarrow=False
             ) 
         fig.update_layout(bargap=0.2)
+        
         return fig
 
-    def pie_chart (self, feature, feature_name, home_value):
-
-        df_room = self.df_district.groupby(feature, as_index=False).count()
-        
-
+    def pie_chart (self, feature: str, feature_name: str, home_value: int) -> None:
+        """
+        Makes a piechart via plotly for counting an attribute.
+        The piechart has a hole in the middle and the piece realted to the house is a bit pulled out for emphasis.
+        """ 
+        # Makes df with that stores the counting of each value of the feature
+        df_room = self.df_district.groupby(feature, as_index=False).count()        
+        # Selects only the columns pertinent to the Count
         df_room = df_room [[feature, "district"]]
+        # Renames them for the plot
         df_room.rename(columns={"district": "Count", feature : feature_name}, inplace= True)        
-
+        # Finds the row of the value that is the same as the house in the prediction
+        # And creates a bool series to know which piece to pull from the pie
         pull = df_room[feature_name] == home_value
         pull = pull * 0.1 
-
+        # Starts pieplot
         fig = go.Figure(
             data=[go.Pie(labels=df_room[feature_name], 
             texttemplate="<b>%{label}</b><br>""%{percent:.1%}",
@@ -79,16 +101,12 @@ class Plots:
                         )
                 ]             
         )
-
-        fig.update_layout(
-            # Add annotations in the center of the donut.
+        # Makes the label inside the hole of the pie
+        fig.update_layout(           
             annotations=[                              
                 dict(
-                    text="<b>{name}</b></br></br>Yours has {home_value}.".format(name=feature_name.upper(), home_value = home_value),
-                 
-                    # Hide the arrow that points to the [x,y] coordinate
-                    showarrow=False
-                
+                    text="<b>{name}</b></br></br>Yours has {home_value}.".format(name=feature_name.upper(), home_value = home_value),                 
+                    showarrow=False                
                 )
             ]
         )
